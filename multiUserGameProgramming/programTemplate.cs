1fcd58cdf8d0b499cfb8e9d2ca7c3e1796833760
S11348091 <111979897+S11348091@users.noreply.github.com>
2023-09-27 12:35:17 -0400

    uduygliguldfuhfdohi;

---
+++ multiUserGameProgramming/programTemplate.cs
@@ -0,0 +1,13 @@
+// Amaure Cooper, Program Template, v0.0
+using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
