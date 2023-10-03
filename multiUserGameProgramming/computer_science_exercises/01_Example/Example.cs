49d0f7e5486b4b67cd0fbac6501e1fba5af6d88c
S11348091 <111979897+S11348091@users.noreply.github.com>
2023-10-03 14:37:38 -0400

    v0.fa

---
+++ multiUserGameProgramming/computer_science_exercises/01_Example/Example.cs
@@ -0,0 +1,41 @@
+// 02_Collections, Amaure Cooper, 11-08-2022 v0.0
+using System;
+
+namespace _02_Collections
+{
+    }
+     class Progarm
+     {
+        static void Main(string[] args) 
+        }
+            // Collections are varations that can store multiple values in one varaible.
+
+            /* Arrays
+            -- Number of elements in an array CANNOT change.
+            -- Contents of elements in an array can change.
+            -- Items in the array are called Elements.
+            -- Array are ordered, meaning each item has a fixed position.
+            -- The position is known as the INDEX.
+            -- First element in an array is index 0.
+            /*
+        
+            // Declaring and Defining an Array
+            string[] breakfastfood = {"Bacon", "Waffles", "Pancakes", "Cereal", "Parfit"};
+            int[] testscores = {95, 100, 25, 15, 27, 35};
+            float[] GPA = {3.14f, 2.25f, 1.74f, 1.99f, 099f, 4.25f};
+
+
+
+
+           Console.WriteLine("Hello World");
+        }
+    }    
+ }
+
+
+
+
+
+
+
+
