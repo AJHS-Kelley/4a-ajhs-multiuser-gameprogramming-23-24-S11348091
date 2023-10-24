3f2666eaeec0be68d084319c4e62031e742c2909
S11348091 <111979897+S11348091@users.noreply.github.com>
2023-10-24 14:33:00 -0400

    programTemple.01

--- multiUserGameProgramming/programTemplate.cs
+++ multiUserGameProgramming/programTemplate.cs
@@ -3,11 +3,43 @@ using System;
 
 namespace Operators
 {
-    class Program
+    class Program MethodsParameters 
     {
-        static void Main(string[] args)
+        // Method -- name block of code, can be used over again.
+        // All methods have a SIGNATURE that defines their names, parameters, and 
+        // Example signature
+        0 reference 
+        static void myMethod()
         {
-
+           Console.writeLine("I Like mine with lettce and tamtos, Heniz 57, and french fries potatoes.\n");
         }
+        // static -- this method belongs to the current class, it is not an object
+        // void -- This method has no return value.
+        
+        0 reference
+        static int Doubleup();
+        {
+           int sum = 0;
+           Console.writeLine("This methoid will double a number and return it.\n");
+           Console.writeLine("Please enetr a number on the next line.\n")
+            sum = System.Convert.ToInt32(Console.ReadLine());
+            //sum = *= 2;
+            //Console.writeLine(sum)
+            return sum *= 2   
+        }
+         
+
+          // Methods with parameters
+
+          
+   
+
+    
+
+        
+    
+
+
+
     }
 }
